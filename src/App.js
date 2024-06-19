8ffa6cbd94ebb6ea07536c724a409fc5d3273191
IrinaWilson <keysii@yandex.ru>
2024-06-19 10:31:08 +1000

    added a Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <footer>
-        This project was coded by Irina Wilson and open-sourced on{" "}
-        <a
-          href="https://github.com/IrinaWilson/shecodes-weather-react-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          This project was coded by Irina Wilson and open-sourced on{" "}
+          <a
+            href="https://github.com/IrinaWilson/shecodes-weather-react-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
