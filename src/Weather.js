e62a002c0ef5b89c4f26b666265813afc1177db9
IrinaWilson <keysii@yandex.ru>
2024-07-09 19:31:27 +1000

    changed padding for responsiveness

--- src/Weather.js
+++ src/Weather.js
@@ -49,7 +49,7 @@ export default function Weather(props) {
               ></input>
             </div>
             <div className="col-3">
-              <input type="submit" value="Search" className="btn btn w-100" />
+              <input type="submit" value="Search" className="btn btn" />
             </div>
           </div>
         </form>
